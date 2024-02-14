1e4ab35a5effc435efe312a8cabfbacb6050a538
Aqib <aqibrashidpandit@gmail.com>
2024-02-14 22:28:39 +0530

    use spatie package configure

--- routes/api.php
+++ routes/api.php
@@ -3,4 +3,6 @@
 use AlNutile\DocusignDriver\Http\Controllers\ElectronicSignatureWebhookController;
 use Illuminate\Support\Facades\Route;
 
-Route::any('/webhooks/electronic_signature_system', [ElectronicSignatureWebhookController::class, 'handleWebhook']);
+Route::middleware('api')->prefix('api')->group(function () {
+    Route::any('/webhooks/electronic_signature_system', [ElectronicSignatureWebhookController::class, 'handleWebhook']);
+});
--- src/DocusignDriverServiceProvider.php
+++ src/DocusignDriverServiceProvider.php
@@ -3,26 +3,12 @@
 namespace AlNutile\DocusignDriver;
 
 use AlNutile\DocusignDriver\Commands\DocusignDriverCommand;
-use Illuminate\Support\Facades\Route;
+
 use Spatie\LaravelPackageTools\Package;
 use Spatie\LaravelPackageTools\PackageServiceProvider;
 
 class DocusignDriverServiceProvider extends PackageServiceProvider
 {
-    /**
-     * Bootstrap any package services.
-     */
-    public function boot(): void
-    {
-        $this->publishes([
-            __DIR__.'/../config/docusigndriver.php' => config_path('docusigndriver.php'),
-        ]);
-
-        Route::middleware('api')->prefix('api')->group(function () {
-            $this->loadRoutesFrom(__DIR__.'/../routes/api.php');
-        });
-    }
-
     public function configurePackage(Package $package): void
     {
         /*
@@ -34,6 +20,7 @@ class DocusignDriverServiceProvider extends PackageServiceProvider
             ->name('docusigndriver')
             ->hasConfigFile()
             ->hasViews()
+            ->hasRoute('api')
             ->hasMigration('create_docusigndriver_table')
             ->hasCommand(DocusignDriverCommand::class);
     }
